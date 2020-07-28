35278440d59f0b68aec554fbef2784a02cdfce28
Chris Marshall <chris@codegoalie.com>
2020-07-28 06:07:49 -0400

    Add heights

---
+++ src/heights.ts
@@ -0,0 +1,130 @@
+export const heights = [
+  // Magic Kingdom
+  {
+    name: "The Barnstormer",
+    height: 35,
+  },
+  {
+    name: "Big Thunder Mountain Railroad",
+    height: 40,
+  },
+  {
+    name: "Seven Dwarfs Mine Train",
+    height: 38,
+  },
+  {
+    name: "Space Mountain",
+    height: 44,
+  },
+  {
+    name: "Splash Mountain",
+    height: 40,
+  },
+  {
+    name: "Tomorrowland Speedway (doubles car)",
+    height: 32,
+  },
+  {
+    name: "Tomorrowland Speedway (own car)",
+    height: 54,
+  },
+
+  // Epcot
+  {
+    name: "Mission: SPACE",
+    height: 40,
+  },
+  {
+    name: "Soarin' Around the World",
+    height: 40,
+  },
+  {
+    name: "Test Track",
+    height: 40,
+  },
+
+  // Hollywood Studios
+  {
+    name: "Alien Swirling Saucers",
+    height: 32,
+  },
+  {
+    name: "Milennium Falcon: Smugglers Run",
+    height: 38,
+  },
+  {
+    name: "Rock 'n' Roller Coaster Starring Aerosmith",
+    height: 48,
+  },
+  {
+    name: "Slinky Dog Dash",
+    height: 38,
+  },
+  {
+    name: "Star Tours - The Adventure Continues",
+    height: 40,
+  },
+  { name: "Star Wars: Rise of the Resistance", height: 40 },
+  {
+    name: "The Twighlight Zone Tower of Terror™",
+    height: 40,
+  },
+
+  // Animal Kingdom
+  {
+    name: "Avatar Flight of Passage",
+    height: 44,
+  },
+  {
+    name: "DONOESAUR",
+    height: 40,
+  },
+  {
+    name: "Expedition Everest - Legend of the Forbidden Mountain",
+    height: 44,
+  },
+  {
+    name: "Kali River Rapids",
+    height: 38,
+  },
+
+  // Typhoon Lagoon
+  {
+    name: "Bay Slides",
+    height: 60,
+  },
+  {
+    name: "Crush 'n' Gusher",
+    height: 48,
+  },
+  {
+    name: "Humunga Kowabunga",
+    height: 48,
+  },
+  {
+    name: "Ketchakiddee Creek",
+    height: 48,
+  },
+
+  // Blizzard Beach
+  {
+    name: "Chairlift",
+    height: 32,
+  },
+  {
+    name: "Downhill Double Dipper",
+    height: 48,
+  },
+  {
+    name: "Slush Gusher",
+    height: 48,
+  },
+  {
+    name: "Summit Plummet",
+    height: 48,
+  },
+  {
+    name: "Tike's Peak",
+    height: 48,
+  },
+];
